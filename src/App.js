c8db367cab7662fe9ba529afa8a8c3990d1891d8
bviala <baptiste.viala@outlook.com>
2019-09-15 19:43:39 +0200

    cleans, adds eslint and react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const fruits = [
+  {
+    name: 'pomme',
+    like: true
+  },
+  {
+    name: 'poire',
+    like: true
+  },
+  {
+    name: 'banane',
+    like: false
+  },
+  {
+    name: 'fraise',
+    like: false
+  },
+  {
+    name: 'cerise',
+    like: true
+  }
+]
+
+function App () {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          J'aime :
+          <ul>
+            {
+              fruits.map(fruit => {
+                if (fruit.like) {
+                  return (
+                    <li>les {fruit.name}s</li>
+                  )
+                } else {
+                  return null
+                }
+              })
+            }
+          </ul>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
