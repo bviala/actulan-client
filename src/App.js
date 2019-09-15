5f96acdd0ff942d2411afd1012e8d83133300d96
bviala <baptiste.viala@outlook.com>
2019-09-15 20:27:17 +0200

    Layout

--- src/App.js
+++ src/App.js
@@ -1,53 +1,45 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-
-const fruits = [
-  {
-    name: 'pomme',
-    like: true
-  },
-  {
-    name: 'poire',
-    like: true
-  },
-  {
-    name: 'banane',
-    like: false
-  },
-  {
-    name: 'fraise',
-    like: false
-  },
-  {
-    name: 'cerise',
-    like: true
-  }
-]
 
 function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          J'aime :
-          <ul>
-            {
-              fruits.map(fruit => {
-                if (fruit.like) {
-                  return (
-                    <li>les {fruit.name}s</li>
-                  )
-                } else {
-                  return null
-                }
-              })
-            }
-          </ul>
-        </p>
+    <div className="root">
+      <header>
+        <h1>
+          Actu-Lan
+        </h1>
       </header>
+      <main>
+        <div class="map-container">
+          lol
+        </div>
+        <div class="event-list">
+          <ul>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>item></li>
+            <li>last item></li>
+          </ul>
+        </div>
+      </main>
     </div>
   )
 }
