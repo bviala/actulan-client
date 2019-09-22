e8adb6fdb07d96565770fba3abcbb16ae33b68a3
bviala <baptiste.viala@outlook.com>
2019-09-22 12:12:14 +0200

    fixes key warning

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -4,7 +4,9 @@ import './EventList.scss'
 function EventList (props) {
   return (
     props.events.map(event => (
-      <div className="columns event is-size-4">
+      <div
+        key={event.id}
+        className="columns event is-size-4">
         <span className="column is-2">{event.date}</span>
         <span className="column has-text-weight-semibold">{event.name}</span>
       </div>
--- src/constants/actuLanEvents.js
+++ src/constants/actuLanEvents.js
@@ -18,109 +18,109 @@ export default [
     slots: 200
   },
   {
-    id: 1,
+    id: 4,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 5,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 6,
     date: '30/10',
     name: 'giga lan',
     slots: 200
   },
   {
-    id: 1,
+    id: 7,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 8,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 9,
     date: '30/10',
     name: 'giga lan',
     slots: 200
   },
   {
-    id: 1,
+    id: 10,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 11,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 12,
     date: '30/10',
     name: 'giga lan',
     slots: 200
   },
   {
-    id: 1,
+    id: 13,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 14,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 32132,
     date: '30/10',
     name: 'giga lan',
     slots: 200
   },
   {
-    id: 1,
+    id: 13209,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 2326,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 3090,
     date: '30/10',
     name: 'giga lan',
     slots: 200
   },
   {
-    id: 1,
+    id: 121,
     date: '20/09',
     name: 'super lan',
     slots: 50
   },
   {
-    id: 2,
+    id: 232080,
     date: '12/10',
     name: 'mega lan',
     slots: 100
   },
   {
-    id: 3,
+    id: 323245,
     date: '30/10',
     name: 'giga lan',
     slots: 200
