fd623e79f76d3bac8ad79be1fde656889f2e6dcc
bviala <baptiste.viala@outlook.com>
2019-09-29 15:41:39 +0200

    Routing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import './index.scss'
+import MapAndEvents from './pages/MapAndEvents'
+
+function App () {
+  return (
+    <div className="root">
+      <header className="navbar is-primary">
+        <Link to='/'>
+          <h1 className='navbar-brand is-size-4'>
+            Actu-Lan
+          </h1>
+        </Link>
+      </header>
+      <main>
+        <Switch>
+          <Route path="/about">
+            <div>lilalalilala</div>
+          </Route>
+          {/* If none of the previous routes render anything,
+              this route acts as a fallback.
+
+              Important: A route with path="/" will *always* match
+              the URL because all URLs begin with a /. So that's
+              why we put this one last of all */}
+          <Route path="/">
+            <MapAndEvents/>
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root'))
---
+++ src/pages/MapAndEvents.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import './MapAndEvents.scss'
+import EventList from '../components/EventList'
+import GoogleMap from '../components/GoogleMap'
+import { getEvents } from '../api/events'
+
+class MapAndEvents extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      events: [],
+      hoveredEvent: null,
+      hoveredMarker: null
+    }
+    this.onEventHoverChange = this.onEventHoverChange.bind(this)
+    this.onMarkerHoverChange = this.onMarkerHoverChange.bind(this)
+  }
+
+  onEventHoverChange (id) {
+    this.setState({
+      hoveredEvent: id
+    })
+  }
+
+  onMarkerHoverChange (id) {
+    this.setState({
+      hoveredMarker: id
+    })
+  }
+
+  componentDidMount () {
+    getEvents().then(events => {
+      this.setState({
+        events
+      })
+    })
+  }
+
+  render () {
+    return (
+      <div className="map-and-events">
+        <GoogleMap
+          events={this.state.events}
+          hoveredEvent={this.state.hoveredEvent}
+          onHoverChange={this.onMarkerHoverChange}/>
+        <div className="event-list-container section">
+          <EventList
+            events={this.state.events}
+            hoveredMarker={this.state.hoveredMarker}
+            onHoverChange={this.onEventHoverChange}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MapAndEvents
